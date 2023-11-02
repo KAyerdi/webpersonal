f306758fac8e6f0bf8d1d14224db716b99b81b71
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-11-02 14:45:57 -0300

    actualizando links repo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ProjectCard from "./ProjectCards";
-import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import { Col, Container, Row } from "react-bootstrap";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import chatify from "../../Assets/Projects/chatify.png";
+import editor from "../../Assets/Projects/codeEditor.png";
+import Particle from "../Particle";
+import ProjectCard from "./ProjectCards";
 
 function Projects() {
   return (
@@ -24,7 +24,7 @@ function Projects() {
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
+              ghLink="https://github.com/KAyerdi/Chatify"
               demoLink="https://chatify-49.web.app/"
             />
           </Col>
@@ -35,7 +35,7 @@ function Projects() {
               isBlog={false}
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              ghLink="https://github.com/KAyerdi/Bits-0f-C0de"
               demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
@@ -46,7 +46,7 @@ function Projects() {
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
+              ghLink="https://github.com/KAyerdi/Editor-io"
               demoLink="https://editor.soumya-jit.tech/"
             />
           </Col>
