0823b610d64a688b248f93b8a9a7ca1312d7c27b
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-11-02 18:19:12 -0300

    actualizando

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV-Kevin.png";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import pdf from "../Assets/CV-Kevin.png"; // https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:c2d3fea4-18d7-46fd-96e4-d6ebf241eb57
+import Particle from "../Particle";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
