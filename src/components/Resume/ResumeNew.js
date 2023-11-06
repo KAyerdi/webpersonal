95e488149a9e0bae4f7d48cba6ba2f2a1cc14528
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-11-06 03:53:47 -0300

    agregando fotos metatag

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import pdf from "../Assets/CV-Kevin.png"; // https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:c2d3fea4-18d7-46fd-96e4-d6ebf241eb57
+import pdf from "../Assets/CV-Kevin.pdf"; // https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:c2d3fea4-18d7-46fd-96e4-d6ebf241eb57
 import Particle from "../Particle";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
