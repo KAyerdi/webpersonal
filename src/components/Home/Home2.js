3fa9b0a3a83a85205d1e6e9063bca0e44acc7341
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-11-06 03:59:11 -0300

    agregado foto a web

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -7,7 +7,7 @@ import {
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import Tilt from "react-parallax-tilt";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/profilePic.jpeg";
 
 function Home2() {
   return (
@@ -54,7 +54,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} className="img-fluid" alt="profilePic" />
             </Tilt>
           </Col>
         </Row>
