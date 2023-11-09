b1fe2a531472e527286667b8393d91824a0fab8c
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-11-09 09:47:24 -0300

    Actualizando cv BUTTOM

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -22,7 +22,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href="https://cv-three-chi.vercel.app/"
+            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -40,7 +40,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href="https://cv-three-chi.vercel.app/"
+            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
