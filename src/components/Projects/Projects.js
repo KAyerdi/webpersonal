2315a921236a67655da728075a747b6ccdfa1468
Kevin Ayerdi <kevin.ayerdi@hotmail.com>
2023-11-24 00:34:30 -0300

    actualizando

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,66 +18,63 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" style={{ whiteSpace: 'pre-line' }}>
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/KAyerdi/Chatify"
-              style={{ whiteSpace: 'pre-line' }}
+              
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" style={{ whiteSpace: 'pre-line' }}>
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
               title="Bits-0f-C0de"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink="https://github.com/KAyerdi/Bits-0f-C0de"
-              style={{ whiteSpace: 'pre-line' }}
+              
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" style={{ whiteSpace: 'pre-line' }}>
             <ProjectCard
               imgPath={editor}
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink="https://github.com/KAyerdi/Editor-io"
-              style={{ whiteSpace: 'pre-line' }}
+              
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" >
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Twitter Clone"
               description="Personal project for educational purposes, created with React.js, TypeScript and some CSS. It has features that allow the user to send messages in real time, share images, and support reactions on messages."
               ghLink="https://github.com/KAyerdi/Twitter-Clone.git"
-              style={{ whiteSpace: 'pre-line' }}
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" style={{ whiteSpace: 'pre-line' }}>
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Minecraft Clone"
               description="This web video game is a personal project for educational purposes, created with HTML, CSS, JavaScript and ThreeJS."
               ghLink="https://github.com/KAyerdi/Minecraft-Clone.git"
-              style={{ whiteSpace: 'pre-line' }}
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card" style={{whiteSpace: "pre-line"}} >
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="ChatGPT Clone"
               description="This is a personal project for educational purposes, created with React and Tailwind."
               ghLink="https://github.com/KAyerdi/ChatGPT-Clone.git"
-              style={{ whiteSpace: 'pre-line' }}
             />
           </Col>
         </Row>
